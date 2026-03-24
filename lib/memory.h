ab7dfbaae5f404191b04dc42e8b4609fa9eddc5d
Dhanush-Poduval <dhanushpoduval@gmail.com>
2026-03-24 14:15:31 +0530

    feat:made the function to enable raw mode

--- lib/memory.h
+++ lib/memory.h
@@ -1,4 +1,7 @@
 #ifndef MEMORY_H
 #define MEMORY_H
-
+#include <termios.h>
+struct termios raw;
+void activate_rawmode(struct termios *raw);
+void deactivate_rawmode(struct termios *raw);
 #endif MEMORY_H
---
+++ src/memory.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "memory.h"
+/*
+ * function to activate raw mode (no need to press enter anymore for the terminal to read characters)
+ * arguments - pointer to the terminal settings pointer so that values are changed to the original
+ * get the current settings changes them using the c_lflag(local flag) item inside and setting it to the opposite of the current bits for echo and ICANON
+ * echo - causes each key i type to be printed in the terminal && icanon - is to enable disable canonical mode
+*/
+struct termios org; //to store the old settings / configuration of the terminal
+void activate_rawmode(struct termios *raw){
+  tcgetattr(STDIN_FILENO,raw);
+  org=*raw;
+  raw->c_lflag &= ~(ECHO | ICANON);
+  tcsetattr(STDIN_FILENO,TCSAFLUSH,raw);
+};
+
